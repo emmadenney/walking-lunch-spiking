732fdd8adb9e14939887b63f5a89d0f5e884813b
emmadenney <emma_denney@hotmail.com>
2023-03-24 09:33:55 +0000

    Map generation

---
+++ walking-lunch-spiking/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map}></MapView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "80%",
+    height: "80%",
+  },
+});
