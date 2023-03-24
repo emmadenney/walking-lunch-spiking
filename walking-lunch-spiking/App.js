0d604535a6f85f16a9cb3d1a25ce18ee19122706
Richard <wisemanrick@hotmail.co.uk>
2023-03-24 12:26:14 +0000

    Directions with waypoints

--- walking-lunch-spiking/App.js
+++ walking-lunch-spiking/App.js
@@ -4,10 +4,17 @@ import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function App() {
   const [location, setLocation] = useState();
   const [address, setAddress] = useState();
+// for directions 
+//const origin = {latitude: 53.4721341, longitude: -2.2377251};// hard coded NC
+const origin = "Manchester Technology Centre"
+const destination = {latitude: 53.636325899999996, longitude: -2.3278136}; //Ricks house
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDIt7GvEhgmT3io-pKMPqTKIif4jkx9-2U';
+// for directions 
 
   useEffect(() => {
     const getPermissions = async () => {
@@ -83,6 +90,16 @@ export default function App() {
           coordinate={{ latitude: 53.3869, longitude: -2.3489 }}
           // ^^ this is laying one hard coded marker rather than mapping over an array of locations to mark
           /> */}
+          <MapViewDirections
+          origin={origin}
+          destination={destination}
+          mode="WALKING"
+          strokeWidth={5}
+          strokeColor="hotpink"
+          waypoints = {["Trafford Centre"]}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+          
         </MapView>
       ) : (
         <Text>Loading...</Text>
